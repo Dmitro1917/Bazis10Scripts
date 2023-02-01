4c3030a031d998556f360900f01999094901d2f6
Dmitro1917 <vinokurov1998@icloud.com>
2023-02-01 17:25:27 +0500

    add 2 scripts

---
+++ Код-позиция.js
@@ -0,0 +1,23 @@
+﻿selObj = Model.Selections
+countSel = Model.SelectionCount
+if (selObj != null) {
+    for (i = 0; i < countSel; ++i) {
+        var n = selObj[i]
+        var pan = n.AsPanel
+        if (n.Owner.Name != "Model") {
+            if (pan) {
+                // n.ArtPos += "." + n.Owner.Name
+                n.Name = n.Owner.Name + " " + n.Name
+            }
+        } else {
+            var count = n.Count
+            for (j = 0; j < count; ++j) {
+                var pan2 = n.Objects[j].AsPanel
+                if (pan2) {
+                    // n.Objects[j].ArtPos += "." + n.Name
+                    n.Objects[j].Name = n.Name + " " + n.Objects[j].Name
+                }
+            }
+        }
+    }
+}
