7999e7e40fc0b5337ff7378d8b8bf07689741fc7
Dmitro1917 <vinokurov1998@icloud.com>
2023-04-21 17:36:52 +0500

    Добавил автоЗаглавнуюБукву в переименовать и скрыть

--- Переименовать и скрыть.js
+++ Переименовать и скрыть.js
@@ -1,21 +1,13 @@
-selObj = Model.Selections
+ÔªøselObj = Model.Selections
 countSel = Model.SelectionCount
-var newName = prompt('»Ïˇ?')
+var newName = prompt('–ò–º—è?')
 
 if (selObj != null) {
     for (i = 0; i < countSel; ++i) {
         var n = selObj[i]
-        //        var num = i + parseInt(number)
-        //        var pan = n.AsPanel
-        //        if (n.Owner.Name != "Model") {
-        //            if (pan) {
-        //            var nom = num - fur
-        if (newName != '') {
-            n.Name = newName
+        if (newName != '' && n.AsPanel) {
+            n.Name = newName[0].toUpperCase() + newName.slice(-newName.length+1);
             n.Visible = False
-            //            } else {
-            //            ++fur
-            //            }
         }
     }
 }
---
+++ Преф-поз.js
@@ -0,0 +1,22 @@
+﻿selObj = Model.Selections
+countSel = Model.SelectionCount
+var newName = prompt('Префикс')
+if (selObj != null) {
+    for (i = 0; i < countSel; ++i) {
+        var n = selObj[i]
+        var pan = n.AsPanel
+        if (n.Owner.Name != "Model") {
+            if (pan) {
+                n.ArtPos = newName + "-" + n.ArtPos
+            }
+        } else {
+            var count = n.Count
+            for (j = 0; j < count; ++j) {
+                var pan2 = n.Objects[j].AsPanel
+                if (pan2) {
+                    n.Objects[j].ArtPos += newName + "-" + n.Objects[j].ArtPos
+                }
+            }
+        }
+    }
+}
