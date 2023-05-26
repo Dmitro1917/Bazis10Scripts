1239c0f9a73ba6329c5b61e1a4bd4b133c0b6157
Dmitro1917 <vinokurov1998@icloud.com>
2023-05-26 16:52:51 +0500

    На момент увольнения

--- Переименовать и скрыть.js
+++ Переименовать и скрыть.js
@@ -5,9 +5,9 @@ var newName = prompt('Имя?')
 if (selObj != null) {
     for (i = 0; i < countSel; ++i) {
         var n = selObj[i]
+        n.Visible = False;
         if (newName != '' && n.AsPanel) {
             n.Name = newName[0].toUpperCase() + newName.slice(-newName.length+1);
-            n.Visible = False
         }
     }
 }
